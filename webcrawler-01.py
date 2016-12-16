e5776fef8fb3e1072699f46bf1d4fb2d7af69b47
Ben A <andersonbe@wilmoths.k12.wi.us>
2016-12-15 18:59:45 -0600

    small add
    stuff changed is commented out, WIP

--- webcrawler-01.py
+++ webcrawler-01.py
@@ -11,6 +11,8 @@ reddit = praw.Reddit(client_id='cr6fA22zW_2ZqA',
                 client_secret='8cw3h11hDyCu3rfKvt4PTpaL-Q0',
                 user_agent='python:ch.memewat:v0.0.1 (by /u/boe6eod7nty')
 
+memes = ['pepe','me irl','dat boi']
+
 try:
     x=0
     postsGrabbed = list(reddit.subreddit('all').hot(limit=10))
@@ -25,6 +27,16 @@ try:
     saveFile.write(saveLine)
     saveFile.close()
     
+    '''
+    n=0
+    title, score = np,loadtxt('data2.csv',
+                              delimiter=',',
+                              unpack=True,
+                              dtype='str')
+    for eachRow in title[:-1]:
+        saveLine = eachRow+','+score[n]+'\n'
+        save
+    '''
 except Exception, e:
     print('TypeError: ',str(e))
 
@@ -33,12 +45,25 @@ except Exception, e:
 for submission in reddit.subreddit('all').hot(limit=10):
     print(submission.title[0:75], '---', submission.score)
 '''
-
 '''
 try:
     date, rate, arb = np.loadtxt('TutSheet.csv',
                                  delimiter=',',
                                  upack=True,
                                  dtype='str')
+        for eachDate in date[x:-1]:
+            plusVar = str((int(arb[x-1]))+(int(arb[x])))
+            
+            saveLine = eachDate+','+rate[x]+','+arb[x]+','+str(int(arb[x])+3)+','+plusVar+'\n'
+            saveFile = open('newCSV.csv','a')
+            saveFile.write(saveLine)
+            saveFile.close()
+            x+=1
+        saveLine = date[-1]+','+rate[x]+','+arb[x]+','+str(int(arb[x])+3)+','+plusVar
+        saveFile = open('newCSV.csv','a')
+        saveFile.write(saveLine)
+        saveFile.close()
+        
+    except Exception, e:
+        print(str(e))
 '''
-
