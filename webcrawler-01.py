f272cdff9643d17b4a6de0425a7c361ec276d354
Ben A <andersonbe@wilmoths.k12.wi.us>
2016-12-15 18:26:25 -0600

    Name saver for reddit
    Iterator to grab titles+scores and store to local csv file

--- webcrawler-01.py
+++ webcrawler-01.py
@@ -1,25 +1,44 @@
 from __future__ import print_function
 import sys
-import pip
+import os
 import praw
 import csv
-import os
+import numpy as np
 
-os.chdir('C:\Users\Benjamin\Python')
+os.chdir('C:\Users\Benjamin\Documents\GitHub\MemeWatch')
 
 reddit = praw.Reddit(client_id='cr6fA22zW_2ZqA',
-                     client_secret='8cw3h11hDyCu3rfKvt4PTpaL-Q0',
-                     user_agent='python:ch.memewat:v0.0.1 (by /u/boe6eod7nty')
+                client_secret='8cw3h11hDyCu3rfKvt4PTpaL-Q0',
+                user_agent='python:ch.memewat:v0.0.1 (by /u/boe6eod7nty')
+
+try:
+    x=0
+    postsGrabbed = list(reddit.subreddit('all').hot(limit=10))
+    for eachPost in postsGrabbed[:-1]:
+        saveLine = eachPost.title+','+str(eachPost.score)+'\n'
+        saveFile = open('data2.csv','a')
+        saveFile.write(saveLine)
+        saveFile.close()
+        x+=1
+    saveLine = postsGrabbed[-1].title+','+str(postsGrabbed[-1].score)
+    saveFile = open('data2.csv','a')
+    saveFile.write(saveLine)
+    saveFile.close()
+    
+except Exception, e:
+    print('TypeError: ',str(e))
+
+
 '''
 for submission in reddit.subreddit('all').hot(limit=10):
     print(submission.title[0:75], '---', submission.score)
 '''
 
-c = csv.writer(open("data.csv", "wb"))
+'''
+try:
+    date, rate, arb = np.loadtxt('TutSheet.csv',
+                                 delimiter=',',
+                                 upack=True,
+                                 dtype='str')
+'''
 
-c.writerow(["Name","Address","Telephone","Fax","E-mail","Others"])
-
-cr = csv.reader(open("data.csv","rb"))
-
-for row in cr:
-    print(row)
