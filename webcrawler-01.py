d73e73166853acd09e587ed020bcac5bf19708bc
Ben A <andersonbe@242-01.local>
2016-12-21 09:58:55 -0600

    advancements
    lmao kill me pls

--- webcrawler-01.py
+++ webcrawler-01.py
@@ -6,16 +6,16 @@ import csv
 import numpy as np
 import time
 
-#os.chdir('/Users/andersonbe/MemeWatch')
+os.chdir('/Users/andersonbe/MemeWatch')
 
 reddit = praw.Reddit(client_id='cr6fA22zW_2ZqA',
                 client_secret='8cw3h11hDyCu3rfKvt4PTpaL-Q0',
                 user_agent='python:ch.memewat:v0.0.1 (by /u/boe6eod7nty')
 
-memes = [['pepe'],['me_irl','me irl','meirl'],['dat boi','dat_boi']]
+memes = [['pepe'],['me_irl','me irl','meirl'],['dat_boi','dat boi']]
 
 try:
-    print(reddit.subreddit('worldnews').title)
+    reddit.subreddit('worldnews').title
 except Exception, e:
     print('TypeError: ',str(e),', Unable to connect to /r/worldnews subreddit')
 else:
@@ -29,7 +29,7 @@ else:
         x=0
         postsGrabbed = list(reddit.subreddit('all').hot(limit=10))
         for eachPost in postsGrabbed[:-1]:
-            saveLine = eachPost.title+','+str(eachPost.score)+'\n'
+            saveLine = '\"'+eachPost.title+'\",'+str(eachPost.score)+'\n'
             saveFile = open('data2.csv','a')
             saveFile.write(saveLine)
             saveFile.close()
@@ -44,8 +44,13 @@ else:
 
 try:
     #creates/re-writes to DAY-HH-MM.csv file, records 
-    currTime = 'records/'+time.strftime('%a-%H-')+str(int(time.strftime('%M'))-(int(time.strftime('%M'))%5))
     
+    currTime = ('records/'
+                +time.strftime('%a-%H-')
+                +str(int(time.strftime('%M'))
+                    -(int(time.strftime('%M'))%5)))
+    
+    #currTime = 'records/temp1'
     n=0
     fileDate = open('%s.csv' % currTime, 'w+')
     title, score = np.genfromtxt('data2.csv',
@@ -56,8 +61,16 @@ try:
     for eachRow in title[:-1]:
         for meme in memes:
             for spelling in meme:
-                if spelling in title:
-                    pass
+                if spelling in title.decode('utf-8').lower():
+                    try:
+                        title.index(meme)
+                    except Exception, e:
+                        saveLine = '\"'+meme[0]+'\",'+title[1]+'\n'
+                        saveFile = open('%s.csv' % currTime,'a')
+                        saveFile.write(saveLine)
+                        saveFile.close()
+                    else:
+                        pass
     
 except Exception, e:
     print('TypeError: ',str(e))
@@ -65,26 +78,4 @@ except Exception, e:
 '''
 for submission in reddit.subreddit('all').hot(limit=10):
     print(submission.title[0:75], '---', submission.score)
-'''
-'''
-try:
-    date, rate, arb = np.loadtxt('TutSheet.csv',
-                                 delimiter=',',
-                                 upack=True,
-                                 dtype='str')
-        for eachDate in date[x:-1]:
-            plusVar = str((int(arb[x-1]))+(int(arb[x])))
-            
-            saveLine = eachDate+','+rate[x]+','+arb[x]+','+str(int(arb[x])+3)+','+plusVar+'\n'
-            saveFile = open('newCSV.csv','a')
-            saveFile.write(saveLine)
-            saveFile.close()
-            x+=1
-        saveLine = date[-1]+','+rate[x]+','+arb[x]+','+str(int(arb[x])+3)+','+plusVar
-        saveFile = open('newCSV.csv','a')
-        saveFile.write(saveLine)
-        saveFile.close()
-        
-    except Exception, e:
-        print(str(e))
 '''
